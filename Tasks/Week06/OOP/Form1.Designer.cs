7b2b82e85821f94cf8b2b69c0074e5bccae17cea
Chingiz Agha <cingiz167@gmail.com>
2021-07-13 19:46:08 +0400

    Some part of Week06 Task01 finished

--- Tasks/Week06/OOP/Form1.Designer.cs
+++ Tasks/Week06/OOP/Form1.Designer.cs
@@ -29,21 +29,211 @@ namespace OOP
         /// </summary>
         private void InitializeComponent()
         {
+            this.comboBoxCategory = new System.Windows.Forms.ComboBox();
+            this.labelCategory = new System.Windows.Forms.Label();
+            this.listViewData = new System.Windows.Forms.ListView();
+            this.comboBoxDelivery = new System.Windows.Forms.ComboBox();
+            this.comboBoxItem = new System.Windows.Forms.ComboBox();
+            this.labelItem = new System.Windows.Forms.Label();
+            this.labelDelivery = new System.Windows.Forms.Label();
+            this.numericUpDownWeight = new System.Windows.Forms.NumericUpDown();
+            this.labelWeight = new System.Windows.Forms.Label();
+            this.numericUpDownPrice = new System.Windows.Forms.NumericUpDown();
+            this.labelPrice = new System.Windows.Forms.Label();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownWeight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPrice)).BeginInit();
             this.SuspendLayout();
+            // 
+            // comboBoxCategory
+            // 
+            this.comboBoxCategory.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxCategory.FormattingEnabled = true;
+            this.comboBoxCategory.Location = new System.Drawing.Point(119, 44);
+            this.comboBoxCategory.Name = "comboBoxCategory";
+            this.comboBoxCategory.Size = new System.Drawing.Size(121, 28);
+            this.comboBoxCategory.TabIndex = 0;
+            // 
+            // labelCategory
+            // 
+            this.labelCategory.AutoSize = true;
+            this.labelCategory.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCategory.Location = new System.Drawing.Point(45, 51);
+            this.labelCategory.Name = "labelCategory";
+            this.labelCategory.Size = new System.Drawing.Size(68, 18);
+            this.labelCategory.TabIndex = 1;
+            this.labelCategory.Text = "Category";
+            // 
+            // listViewData
+            // 
+            this.listViewData.HideSelection = false;
+            this.listViewData.Location = new System.Drawing.Point(366, 98);
+            this.listViewData.Name = "listViewData";
+            this.listViewData.Size = new System.Drawing.Size(348, 268);
+            this.listViewData.TabIndex = 2;
+            this.listViewData.UseCompatibleStateImageBehavior = false;
+            this.listViewData.View = System.Windows.Forms.View.Details;
+            // 
+            // comboBoxDelivery
+            // 
+            this.comboBoxDelivery.FormattingEnabled = true;
+            this.comboBoxDelivery.Location = new System.Drawing.Point(119, 190);
+            this.comboBoxDelivery.Name = "comboBoxDelivery";
+            this.comboBoxDelivery.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxDelivery.TabIndex = 3;
+            // 
+            // comboBoxItem
+            // 
+            this.comboBoxItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxItem.FormattingEnabled = true;
+            this.comboBoxItem.Location = new System.Drawing.Point(119, 98);
+            this.comboBoxItem.Name = "comboBoxItem";
+            this.comboBoxItem.Size = new System.Drawing.Size(121, 28);
+            this.comboBoxItem.TabIndex = 4;
+            // 
+            // labelItem
+            // 
+            this.labelItem.AutoSize = true;
+            this.labelItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelItem.Location = new System.Drawing.Point(44, 98);
+            this.labelItem.Name = "labelItem";
+            this.labelItem.Size = new System.Drawing.Size(41, 20);
+            this.labelItem.TabIndex = 5;
+            this.labelItem.Text = "Item";
+            // 
+            // labelDelivery
+            // 
+            this.labelDelivery.AutoSize = true;
+            this.labelDelivery.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDelivery.Location = new System.Drawing.Point(44, 188);
+            this.labelDelivery.Name = "labelDelivery";
+            this.labelDelivery.Size = new System.Drawing.Size(64, 20);
+            this.labelDelivery.TabIndex = 6;
+            this.labelDelivery.Text = "Delivery";
+            // 
+            // numericUpDownWeight
+            // 
+            this.numericUpDownWeight.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDownWeight.Location = new System.Drawing.Point(119, 141);
+            this.numericUpDownWeight.Name = "numericUpDownWeight";
+            this.numericUpDownWeight.Size = new System.Drawing.Size(120, 26);
+            this.numericUpDownWeight.TabIndex = 7;
+            // 
+            // labelWeight
+            // 
+            this.labelWeight.AutoSize = true;
+            this.labelWeight.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelWeight.Location = new System.Drawing.Point(44, 141);
+            this.labelWeight.Name = "labelWeight";
+            this.labelWeight.Size = new System.Drawing.Size(59, 20);
+            this.labelWeight.TabIndex = 8;
+            this.labelWeight.Text = "Weight";
+            // 
+            // numericUpDownPrice
+            // 
+            this.numericUpDownPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDownPrice.Location = new System.Drawing.Point(119, 241);
+            this.numericUpDownPrice.Name = "numericUpDownPrice";
+            this.numericUpDownPrice.Size = new System.Drawing.Size(120, 26);
+            this.numericUpDownPrice.TabIndex = 9;
+            // 
+            // labelPrice
+            // 
+            this.labelPrice.AutoSize = true;
+            this.labelPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPrice.Location = new System.Drawing.Point(53, 239);
+            this.labelPrice.Name = "labelPrice";
+            this.labelPrice.Size = new System.Drawing.Size(44, 20);
+            this.labelPrice.TabIndex = 10;
+            this.labelPrice.Text = "Price";
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker.Location = new System.Drawing.Point(21, 289);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(218, 22);
+            this.dateTimePicker.TabIndex = 11;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(152, 337);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(88, 29);
+            this.btnAdd.TabIndex = 12;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSearch.Location = new System.Drawing.Point(366, 47);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(153, 26);
+            this.txtSearch.TabIndex = 13;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(599, 47);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(115, 37);
+            this.btnSearch.TabIndex = 14;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.labelPrice);
+            this.Controls.Add(this.numericUpDownPrice);
+            this.Controls.Add(this.labelWeight);
+            this.Controls.Add(this.numericUpDownWeight);
+            this.Controls.Add(this.labelDelivery);
+            this.Controls.Add(this.labelItem);
+            this.Controls.Add(this.comboBoxItem);
+            this.Controls.Add(this.comboBoxDelivery);
+            this.Controls.Add(this.listViewData);
+            this.Controls.Add(this.labelCategory);
+            this.Controls.Add(this.comboBoxCategory);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Online Sifarish";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownWeight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPrice)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxCategory;
+        private System.Windows.Forms.Label labelCategory;
+        private System.Windows.Forms.ListView listViewData;
+        private System.Windows.Forms.ComboBox comboBoxDelivery;
+        private System.Windows.Forms.ComboBox comboBoxItem;
+        private System.Windows.Forms.Label labelItem;
+        private System.Windows.Forms.Label labelDelivery;
+        private System.Windows.Forms.NumericUpDown numericUpDownWeight;
+        private System.Windows.Forms.Label labelWeight;
+        private System.Windows.Forms.NumericUpDown numericUpDownPrice;
+        private System.Windows.Forms.Label labelPrice;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
     }
 }
 
